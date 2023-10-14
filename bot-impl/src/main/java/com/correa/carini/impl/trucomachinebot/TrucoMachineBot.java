c324d00d0ddb880d7aa941c607e28af1404d3385
Eduardo <eduardocorreass25@gmail.com>
2023-10-14 18:27:10 -0300

    feat: implements getFirstRoundCard method to be used in chooseCard

--- bot-impl/src/main/java/com/correa/carini/impl/trucomachinebot/TrucoMachineBot.java
+++ bot-impl/src/main/java/com/correa/carini/impl/trucomachinebot/TrucoMachineBot.java
@@ -2,7 +2,11 @@ package com.correa.carini.impl.trucomachinebot;
 
 import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 import com.bueno.spi.service.BotServiceProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrucoMachineBot implements BotServiceProvider {
     @Override
@@ -17,11 +21,63 @@ public class TrucoMachineBot implements BotServiceProvider {
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
-        return null;
+        int round = intel.getRoundResults().size();
+
+        CardToPlay cardToPlay = null;
+
+        if(round == 0) cardToPlay = CardToPlay.of(getFirstRoundCard(intel));
+
+
+
+        return cardToPlay;
     }
 
     @Override
     public int getRaiseResponse(GameIntel intel) {
         return 0;
+    }
+
+    private TrucoCard getFirstRoundCard(GameIntel intel) {
+        List<TrucoCard> cards = new ArrayList<>(intel.getCards());
+        TrucoCard vira  = intel.getVira();
+
+        TrucoCard greatestCard = getGreatestCard(cards, vira);
+
+        if(intel.getOpponentCard().isPresent()){
+            if(greatestCard.compareValueTo(intel.getOpponentCard().get(), vira) > 0){
+                return greatestCard;
+            }
+
+            return getLowestCard(cards, vira);
+        }
+
+
+        return greatestCard;
+    }
+
+    private TrucoCard getGreatestCard( List<TrucoCard> cards, TrucoCard vira){
+        TrucoCard greatestCard = cards.get(0);
+
+        for (TrucoCard card : cards) {
+            int comparison = card.compareValueTo(greatestCard, vira);
+            if (comparison > 0) {
+                greatestCard = card;
+            }
+        }
+
+        return greatestCard;
+    }
+
+    private TrucoCard getLowestCard( List<TrucoCard> cards, TrucoCard vira){
+        TrucoCard lowestCard = cards.get(0);
+
+        for (TrucoCard card : cards) {
+            int comparison = card.compareValueTo(lowestCard, vira);
+            if (comparison < 0) {
+                lowestCard = card;
+            }
+        }
+
+        return lowestCard;
     }
 }
