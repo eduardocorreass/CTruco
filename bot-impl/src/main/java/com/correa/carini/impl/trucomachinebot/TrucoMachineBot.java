fe1159671e9d9e68cd82ab99fae321c32469d85c
Marcos Vinicius <marcos.carini22@gmail.com>
2023-10-14 21:32:35 -0300

    feat: implement first version of method getRaiseResponse

--- bot-impl/src/main/java/com/correa/carini/impl/trucomachinebot/TrucoMachineBot.java
+++ bot-impl/src/main/java/com/correa/carini/impl/trucomachinebot/TrucoMachineBot.java
@@ -1,5 +1,6 @@
 package com.correa.carini.impl.trucomachinebot;
 
+import com.bueno.spi.model.CardRank;
 import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
 import com.bueno.spi.model.TrucoCard;
@@ -9,6 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TrucoMachineBot implements BotServiceProvider {
+
+    private final List<CardRank> strongCards = List.of(CardRank.ACE, CardRank.TWO,CardRank.THREE);
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
         return false;
@@ -34,7 +37,25 @@ public class TrucoMachineBot implements BotServiceProvider {
 
     @Override
     public int getRaiseResponse(GameIntel intel) {
-        return 0;
+        List<GameIntel.RoundResult> roundResults = intel.getRoundResults();
+        List<TrucoCard> cards = intel.getCards();
+        TrucoCard vira  = intel.getVira();
+
+        if(hasZapCopas(intel)) return 1;
+
+        if (hasZapManilha(intel)){
+            int round = intel.getRoundResults().size();
+
+            // Se for a primeira rodada eu só aceito
+            if (round == 0) return 0;
+            // Se for a segunda rodada, aceito e peço 6
+            if (round == 1) return 1;
+        }
+
+
+        if (hasTwoManilhas(intel)) return 0;
+
+        return -1;
     }
 
     private TrucoCard getFirstRoundCard(GameIntel intel) {
@@ -80,4 +101,58 @@ public class TrucoMachineBot implements BotServiceProvider {
 
         return lowestCard;
     }
+
+    private boolean hasSrongCards(GameIntel intel){
+        return intel.getCards().stream().anyMatch(card -> strongCards.contains(card.getRank()));
+    }
+
+    private boolean hasZapCopas(GameIntel intel){
+        boolean zap = false;
+        boolean copas = false;
+        for (TrucoCard card : intel.getCards()){
+            if (card.isZap(intel.getVira())){
+                zap = true;
+            }
+
+            if (card.isCopas(intel.getVira())){
+                copas = true;
+            }
+        }
+
+        if (zap && copas) return true;
+        return false;
+    }
+
+
+    private boolean hasTwoManilhas(GameIntel intel){
+        int manilhas = 0;
+
+        for (TrucoCard card : intel.getCards()){
+            if (card.isManilha(intel.getVira())){
+                manilhas += 1;
+            }
+        }
+
+        if (manilhas >= 2) return true;
+        return false;
+    }
+
+    private boolean hasZapManilha(GameIntel intel){
+        boolean zap = false;
+        int manilhas = 0;
+
+        for (TrucoCard card : intel.getCards()){
+            if (card.isZap(intel.getVira())){
+                zap = true;
+            }
+            if (card.isOuros(intel.getVira()) || card.isEspadilha(intel.getVira()) || card.isCopas(intel.getVira())){
+                manilhas += 1;
+            }
+        }
+
+        if (zap && manilhas >= 1) return true;
+        return false;
+    }
 }
+
+
